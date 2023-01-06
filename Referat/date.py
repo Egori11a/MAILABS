ba95e3819007212fb97d2addbd895fc5401b81b8
Egori11a <114509561+Egori11a@users.noreply.github.com>
2023-01-06 12:56:08 +0300

    Add files via upload

---
+++ Referat/date.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import telebot
+from pycbrf import ExchangeRates
+
+bot = telebot.TeleBot('5552972722:AAEJAw_4DQZb1m7bRnlv5GitK4iU82opv14')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = telebot.types.KeyboardButton('USD')
+    itembtn2 = telebot.types.KeyboardButton('EUR')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(chat_id=message.chat.id,
+                     text="<b>Hello! Choose the currency to see Exchange Rates!</b>", reply_markup=markup,
+                     parse_mode="html")
+@bot.message_handler(content_types=['text'])
+def message(message):
+    message_norm = message.text.strip().lower()
+    if message_norm in ['usd', 'eur']:
+        rates = ExchangeRates(datetime.now())
+        bot.send_message(chat_id=message.chat.id,
+                         text=f"<b>{message_norm.upper()} rate is {float(rates[message_norm.upper()].rate)}</b>",
+                         parse_mode="html")
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text=f"<b>I dont know this words :( </b>",
+                         parse_mode="html")
+
+bot.polling(none_stop=True)
